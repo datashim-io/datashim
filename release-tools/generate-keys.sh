5ab9e676b3393bd7c721f0d7aaa533dbbb05b0b8
Christian Pinto <christian.pinto@ibm.com>
2020-02-20 11:02:31 +0000

    Fixed mutating webhook certificate creation
    CN of the certificate is now including the installation namespace

--- release-tools/generate-keys.sh
+++ release-tools/generate-keys.sh
@@ -25,6 +25,7 @@
 : ${1?'missing key directory'}
 
 key_dir="$1"
+namespace=$2
 
 chmod 0700 "$key_dir"
 cd "$key_dir"
@@ -34,5 +35,5 @@ openssl req -nodes -new -x509 -keyout ca.key -out ca.crt -subj "/CN=Admission Co
 # Generate the private key for the webhook server
 openssl genrsa -out webhook-server-tls.key 2048
 # Generate a Certificate Signing Request (CSR) for the private key, and sign it with the private key of the CA.
-openssl req -new -key webhook-server-tls.key -subj "/CN=webhook-server.default.svc" \
+openssl req -new -key webhook-server-tls.key -subj "/CN=webhook-server.${namespace}.svc" \
     | openssl x509 -req -CA ca.crt -CAkey ca.key -CAcreateserial -out webhook-server-tls.crt
