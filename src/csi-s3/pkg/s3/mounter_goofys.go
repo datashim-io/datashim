c9975e55a930ac43d6ae8b63f9b18ce8360b9989
Yiannis Gkoufas <yiannisg@ie.ibm.com>
2020-07-21 14:42:18 +0100

    goofys binary installed

--- src/csi-s3/pkg/s3/mounter_goofys.go
+++ src/csi-s3/pkg/s3/mounter_goofys.go
@@ -1,14 +1,7 @@
 package s3
 
 import (
-	"fmt"
 	"github.com/golang/glog"
-	"github.com/kahing/goofys/api/common"
-	"os"
-
-	"context"
-
-	goofysApi "github.com/kahing/goofys/api"
 )
 
 const (
@@ -60,26 +53,26 @@ func (goofys *goofysMounter) Mount(source string, target string) error {
 	//	},
 	//}
 
-	goofysCfg := &common.FlagStorage{
-			MountPoint: target,
-			Endpoint:   goofys.endpoint,
-			DirMode:    0755,
-			FileMode:   0644,
-			MountOptions: map[string]string{
-				"allow_other": "",
-			},
-	}
+	//goofysCfg := &common.FlagStorage{
+	//		MountPoint: target,
+	//		Endpoint:   goofys.endpoint,
+	//		DirMode:    0755,
+	//		FileMode:   0644,
+	//		MountOptions: map[string]string{
+	//			"allow_other": "",
+	//		},
+	//}
 
 	glog.V(3).Infof("Mounting using goofys!")
 
-	os.Setenv("AWS_ACCESS_KEY_ID", goofys.accessKeyID)
-	os.Setenv("AWS_SECRET_ACCESS_KEY", goofys.secretAccessKey)
-	fullPath := fmt.Sprintf("%s:%s", goofys.bucket.Name, "/") //TODO check if it works with goofys correctly
+	//os.Setenv("AWS_ACCESS_KEY_ID", goofys.accessKeyID)
+	//os.Setenv("AWS_SECRET_ACCESS_KEY", goofys.secretAccessKey)
+	//fullPath := fmt.Sprintf("%s:%s", goofys.bucket.Name, "/") //TODO check if it works with goofys correctly
 
-	_, _, err := goofysApi.Mount(context.Background(), fullPath, goofysCfg)
+	//_, _, err := goofysApi.Mount(context.Background(), fullPath, goofysCfg)
 
-	if err != nil {
-		return fmt.Errorf("Error mounting via goofys: %s", err)
-	}
+	//if err != nil {
+	//	return fmt.Errorf("Error mounting via goofys: %s", err)
+	//}
 	return nil
 }
