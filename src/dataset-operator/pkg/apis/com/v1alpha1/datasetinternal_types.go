1a9acea134b501c0e1f5c994cebc773f16026f37
Panos Koutsovasilis <koutsovasilis.panagiotis1@ibm.com>
2021-01-28 09:18:33 +0000

    Introduce DatasetInternal Status Caching
    Signed-off-by: Panagiotis Koutsovasilis <Koutsovasilis.Panagiotis1@ibm.com>

--- src/dataset-operator/pkg/apis/com/v1alpha1/datasetinternal_types.go
+++ src/dataset-operator/pkg/apis/com/v1alpha1/datasetinternal_types.go
@@ -7,11 +7,30 @@ import (
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
+// +k8s:openapi-gen=true
+type CachingPlacementInfo struct {
+	Key   string `json:"key,omitempty"`
+	Value string `json:"value,omitempty"`
+}
+
+// +k8s:openapi-gen=true
+type CachingPlacement struct {
+	Gateways      []CachingPlacementInfo `json:"gateways,omitempty"`
+	DataLocations []CachingPlacementInfo `json:"datalocations,omitempty"`
+}
+
+// +k8s:openapi-gen=true
+type DatasetInternalStatusCaching struct {
+	Placements CachingPlacement `json:"placements,omitempty"`
+}
+
 // DatasetInternalStatus defines the observed state of DatasetInternal
+// +k8s:openapi-gen=true
 type DatasetInternalStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
 	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+	Caching DatasetInternalStatusCaching `json:"caching,omitempty"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
@@ -23,7 +42,7 @@ type DatasetInternal struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	Spec   DatasetSpec   `json:"spec,omitempty"`
+	Spec   DatasetSpec           `json:"spec,omitempty"`
 	Status DatasetInternalStatus `json:"status,omitempty"`
 }
 
