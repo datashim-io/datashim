1246367447baee778488c242ec6c146eae24521a
Tommy Li <Tommy.chaoping.li@ibm.com>
2021-07-12 18:13:44 -0700

    Update archive_handler.go
    Signed-off-by: tomcli <tommy.chaoping.li@ibm.com>

--- src/dataset-operator/pkg/controller/dataset/archive_handler.go
+++ src/dataset-operator/pkg/controller/dataset/archive_handler.go
@@ -23,6 +23,7 @@ func getPodDataDownload(dataset *comv1alpha1.Dataset, operatorNamespace string) 
 		"wget " + fileUrl + " -P" + " /tmp && " +
 		"tar -xf /tmp/" + fileName + " -C /data/" + podId +" && "+
 		"rm -rf /tmp/" + fileName + " && "+
+		"aws s3api create-bucket --bucket "+ podId +" --endpoint-url $ENDPOINT && "+
 		"aws s3 cp /data/" + podId +" s3://"+podId+" --recursive --endpoint-url $ENDPOINT && "+
 		"rm -rf /data",
 	}
