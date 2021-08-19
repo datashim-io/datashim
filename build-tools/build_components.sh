37cfefd08b2b7393625f1554d3a609f594c7b835
Panagiotis Koutsovasilis <Koutsovasilis.Panagiotis1@ibm.com>
2021-08-19 15:09:37 +0300

    identify i386 (mac os x) as amd64

--- build-tools/build_components.sh
+++ build-tools/build_components.sh
@@ -8,5 +8,6 @@ fi
 
 export ARCH=`arch`;
 if [ "$ARCH" == "x86_64" ]; then export ARCH="amd64"; fi
+if [ "$ARCH" == "i386" ]; then export ARCH="amd64"; fi
 if [ "$ARCH" == "aarch64" ]; then export ARCH="arm64"; fi
 make ARCH=$ARCH DOCKER_REGISTRY=$DOCKER_REGISTRY build-components push-components
