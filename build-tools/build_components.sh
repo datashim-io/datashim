ce72ef628722c9a8fe90b2587bab911bde75a131
Panagiotis Koutsovasilis <Koutsovasilis.Panagiotis1@ibm.com>
2021-09-08 13:37:17 +0300

    add travis stages for master, releases and PRs

--- build-tools/build_components.sh
+++ build-tools/build_components.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-if [ -z "$DOCKER_REGISTRY" ]
+MAKE_ARGS="build-components"
+
+if [ -n "$DOCKER_REGISTRY" ]
 then
-      echo "please specify \$DOCKER_REGISTRY"
-      exit 1
+      MAKE_ARGS+=" push-components"
+else
+      DOCKER_REGISTRY="local"
 fi
 
 export ARCH=`arch`;
 if [ "$ARCH" == "x86_64" ]; then export ARCH="amd64"; fi
 if [ "$ARCH" == "i386" ]; then export ARCH="amd64"; fi
 if [ "$ARCH" == "aarch64" ]; then export ARCH="arm64"; fi
-make ARCH=$ARCH DOCKER_REGISTRY=$DOCKER_REGISTRY build-components push-components
+make ARCH=$ARCH DOCKER_REGISTRY=$DOCKER_REGISTRY $MAKE_ARGS
