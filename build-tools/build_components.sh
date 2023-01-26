7669fd883aa6e8f871f15a76b23b80f3e2c6a166
Alessandro Pomponio <alessandro.pomponio1@ibm.com>
2023-01-26 11:05:22 +0000

    Update build_components to use new build scripts
    Signed-off-by: Alessandro Pomponio <alessandro.pomponio1@ibm.com>

--- build-tools/build_components.sh
+++ build-tools/build_components.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
-MAKE_ARGS="build-components"
+print_usage() {
+    echo "Usage: $0 [-p]"
+    echo "Use -p to build and push multiarch images"
+}
 
-if [ -n "$DOCKER_REGISTRY" ]
-then
-      MAKE_ARGS+=" push-components"
+BUILD_AND_PUSH="no"
+while getopts 'p' OPTION
+do
+    case "$OPTION" in
+        p)
+            BUILD_AND_PUSH="yes"
+            ;;
+        ?)
+            print_usage >&2
+            exit 1
+            ;;
+    esac
+done
+
+if [ $BUILD_AND_PUSH = "yes" ]; then
+      cd ../src/dataset-operator && ./build_and_push_multiarch_dataset_operator.sh
+      cd ../src/generate-keys && ./build_and_push_multiarch_generate_keys.sh
 else
-      DOCKER_REGISTRY="local"
+      cd ../src/dataset-operator && ./build_dataset_operator.sh
+      cd ../src/generate-keys && ./build_generate_keys.sh
 fi
-
-export ARCH=`arch`;
-if [ "$ARCH" == "x86_64" ]; then export ARCH="amd64"; fi
-if [ "$ARCH" == "i386" ]; then export ARCH="amd64"; fi
-if [ "$ARCH" == "aarch64" ]; then export ARCH="arm64"; fi
-make ARCH=$ARCH DOCKER_REGISTRY=$DOCKER_REGISTRY $MAKE_ARGS
