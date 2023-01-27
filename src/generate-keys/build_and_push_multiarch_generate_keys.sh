6c96dfe4a61719ab977affc7387d54e95329f54a
Alessandro Pomponio <alessandro.pomponio1@ibm.com>
2023-01-27 09:18:22 +0000

    Fix use of REGISTRY_URL
    Signed-off-by: Alessandro Pomponio <alessandro.pomponio1@ibm.com>

--- src/generate-keys/build_and_push_multiarch_generate_keys.sh
+++ src/generate-keys/build_and_push_multiarch_generate_keys.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 REGISTRY_URL="${1:-quay.io/datashim-io}"
-docker buildx build --platform linux/amd64,linux/arm64,linux/ppc64le --push -t ${1}/generate-keys .
+docker buildx build --platform linux/amd64,linux/arm64,linux/ppc64le --push -t ${REGISTRY_URL}/generate-keys .
--- src/generate-keys/build_generate_keys.sh
+++ src/generate-keys/build_generate_keys.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 REGISTRY_URL="${1:-quay.io/datashim-io}"
-docker build -t ${1}/generate-keys .
+docker build -t ${REGISTRY_URL}/generate-keys .
