f3d060e60b0eef6e183c788e0ca31b957f948edc
SRIKUMAR VENUGOPAL <srikumarv@ie.ibm.com>
2024-01-05 14:33:32 +0000

    adding older group name for debugging
    Signed-off-by: SRIKUMAR VENUGOPAL <srikumarv@ie.ibm.com>

--- src/dataset-operator/api/v1alpha1/groupversion_info.go
+++ src/dataset-operator/api/v1alpha1/groupversion_info.go
@@ -26,7 +26,8 @@ import (
 
 var (
 	// GroupVersion is group version used to register these objects
-	GroupVersion = schema.GroupVersion{Group: "datashim.io", Version: "v1alpha1"}
+	GroupVersion    = schema.GroupVersion{Group: "datashim.io", Version: "v1alpha1"}
+	GroupVersionOld = schema.GroupVersion{Group: "com.ie.ibm.hpsys", Version: "v1alpha1"}
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
 	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
