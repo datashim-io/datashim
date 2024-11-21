e81a0243031a8b554da02855ec2405ae11bc80fb
SRIKUMAR VENUGOPAL <srikumarv@ie.ibm.com>
2024-11-21 23:44:47 +0000

    using docker buildx for csi plugins
    Signed-off-by: SRIKUMAR VENUGOPAL <srikumarv@ie.ibm.com>

--- build-tools/build_csi_plugins.sh
+++ build-tools/build_csi_plugins.sh
@@ -1,33 +1,76 @@
 #!/bin/bash
-
 set -e
 
 print_usage() {
-    echo "Usage: $0 [-p]"
+    echo "Usage: $0 [-k] [-p] [-s]"
+    echo "Use -k to avoid creating another buildx context"
     echo "Use -p to build and push multiarch images"
+    echo "Use -s to skip logging in to the container registry"
 }
 
-MAKE_ARGS="build-csi-plugins"
-
-while getopts 'p' PUSH
+BUILD_AND_PUSH="no"
+CREATE_NEW_BUILDX_CONTEXT="yes"
+SKIP_LOGIN="no"
+while getopts 'kps' OPTION
 do
-   case "$PUSH" in
-      p)
-        MAKE_ARGS+=" push-csi-plugins" 
-        ;;
-   esac
+    case "$OPTION" in
+        k)
+            CREATE_NEW_BUILDX_CONTEXT="no"
+            ;;
+        p)
+            BUILD_AND_PUSH="yes"
+            ;;
+        s)
+            SKIP_LOGIN="yes"
+            ;;
+        ?)
+            print_usage >&2
+            exit 1
+            ;;
+    esac
 done
 
-if [ -n "$DOCKER_REGISTRY" ]
+shift $((OPTIND-1))
+
+REGISTRY_URL="${1:-quay.io/datashim-io}"
+VERSION="${2:-latest}"
+
+DOCKERCMD="docker"
+ALTDOCKERCMD="podman"
+if !(command -v ${DOCKERCMD} &> /dev/null)
 then
-      #MAKE_ARGS+=" push-csi-plugins"
-      :
+    echo "Docker command not found"
+    if !(command -v ${ALTDOCKERCMD} &> /dev/null)
+    then
+        echo "Neither ${DOCKERCMD} nor ${ALTDOCKERCMD} commands found.. cannot build "
+        exit 1
+    else
+        DOCKERCMD=${ALTDOCKERCMD}  
+    fi
 else
-      DOCKER_REGISTRY="local"
+    echo "Docker command found"
+    cmd_type=$(type -t ${DOCKERCMD})
+    if [ $cmd_type == "alias" ]
+    then
+        echo "${DOCKERCMD} is an alias, switching to ${ALTDOCKERCMD}"
+        DOCKERCMD=${ALTDOCKERCMD}  
+    fi
+fi 
+
+if [ ${DOCKERCMD} == "docker" ]
+then 
+    if [ $CREATE_NEW_BUILDX_CONTEXT = "yes" ]; then
+        docker buildx create --use
+    fi
 fi
 
-export ARCH=`arch`;
-if [ "$ARCH" == "x86_64" ]; then export ARCH="amd64"; fi
-if [ "$ARCH" == "i386" ]; then export ARCH="amd64"; fi
-if [ "$ARCH" == "aarch64" ]; then export ARCH="arm64"; fi
-make ARCH=$ARCH DOCKER_REGISTRY=$DOCKER_REGISTRY $MAKE_ARGS
+if [ $BUILD_AND_PUSH = "yes" ]; then
+      if [ $SKIP_LOGIN = "no" ]; then
+            echo $REGISTRY_PASSWORD | docker login -u $REGISTRY_USERNAME --password-stdin $REGISTRY_URL
+      fi
+      (cd ../src/csi-s3 && ./build_multiarch_csis3.sh -p $REGISTRY_URL $VERSION)
+      (cd ../src/csi-nfs && ./build_multiarch_csinfs.sh -p $REGISTRY_URL $VERSION)
+else
+      (cd ../src/csi-s3 && ./build_multiarch_csis3.sh $REGISTRY_URL $VERSION)
+      (cd ../src/csi-nfs && ./build_multiarch_csinfs.sh $REGISTRY_URL $VERSION)
+fi
