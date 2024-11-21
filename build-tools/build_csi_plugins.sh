c190f991c2accb1a98faa68f984330667dd3f008
SRIKUMAR VENUGOPAL <srikumarv@ie.ibm.com>
2024-11-21 23:50:22 +0000

    fix: wrong script name
    Signed-off-by: SRIKUMAR VENUGOPAL <srikumarv@ie.ibm.com>

--- build-tools/build_csi_plugins.sh
+++ build-tools/build_csi_plugins.sh
@@ -68,9 +68,9 @@ if [ $BUILD_AND_PUSH = "yes" ]; then
       if [ $SKIP_LOGIN = "no" ]; then
             echo $REGISTRY_PASSWORD | docker login -u $REGISTRY_USERNAME --password-stdin $REGISTRY_URL
       fi
-      (cd ../src/csi-s3 && ./build_multiarch_csis3.sh -p $REGISTRY_URL $VERSION)
-      (cd ../src/csi-nfs && ./build_multiarch_csinfs.sh -p $REGISTRY_URL $VERSION)
+      (cd ../src/csi-s3 && ./build_and_push_multiarch_csis3.sh -p $REGISTRY_URL $VERSION)
+      (cd ../src/csi-nfs && ./build_and_push_multiarch_csinfs.sh -p $REGISTRY_URL $VERSION)
 else
-      (cd ../src/csi-s3 && ./build_multiarch_csis3.sh $REGISTRY_URL $VERSION)
-      (cd ../src/csi-nfs && ./build_multiarch_csinfs.sh $REGISTRY_URL $VERSION)
+      (cd ../src/csi-s3 && ./build_and_push_multiarch_csis3.sh $REGISTRY_URL $VERSION)
+      (cd ../src/csi-nfs && ./build_and_push_multiarch_csinfs.shh $REGISTRY_URL $VERSION)
 fi
