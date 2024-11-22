6ce95c32df79700d8e7faf00138828bee34d3935
Srikumar Venugopal <srikumar003@users.noreply.github.com>
2024-11-22 00:20:36 +0000

    Updating CSI S3 Docker image to go v1.22 and Debian bookworm (#389)
    * Updating CSI S3 Docker image to go v1.22 and Debian bookworm
    
    Signed-off-by: SRIKUMAR VENUGOPAL <srikumarv@ie.ibm.com>
    
    * Adding additional step of building and loading CSI plugins before integration test
    
    Signed-off-by: SRIKUMAR VENUGOPAL <srikumarv@ie.ibm.com>
    
    * fix: regression in builder for CSI S3
    
    Signed-off-by: SRIKUMAR VENUGOPAL <srikumarv@ie.ibm.com>
    
    * fix: regression in csi-s3 Makefile
    
    Signed-off-by: SRIKUMAR VENUGOPAL <srikumarv@ie.ibm.com>
    
    * fix: setting docker registry variable
    
    Signed-off-by: SRIKUMAR VENUGOPAL <srikumarv@ie.ibm.com>
    
    * fix: setting docker registry variable
    
    Signed-off-by: SRIKUMAR VENUGOPAL <srikumarv@ie.ibm.com>
    
    * using docker buildx for csi plugins
    
    Signed-off-by: SRIKUMAR VENUGOPAL <srikumarv@ie.ibm.com>
    
    * fix: wrong script name
    
    Signed-off-by: SRIKUMAR VENUGOPAL <srikumarv@ie.ibm.com>
    
    * fix: update to build script
    
    Signed-off-by: SRIKUMAR VENUGOPAL <srikumarv@ie.ibm.com>
    
    * fix: update to build csi script
    
    Signed-off-by: SRIKUMAR VENUGOPAL <srikumarv@ie.ibm.com>
    
    * fix: update to build csi script
    
    Signed-off-by: SRIKUMAR VENUGOPAL <srikumarv@ie.ibm.com>
    
    ---------
    
    Signed-off-by: SRIKUMAR VENUGOPAL <srikumarv@ie.ibm.com>

--- src/csi-s3/build_and_push_multiarch_csis3.sh
+++ src/csi-s3/build_and_push_multiarch_csis3.sh
@@ -1,5 +1,85 @@
 #!/bin/bash
+set -e
+
+print_usage() {
+    echo "usage: $0 [-p] <REGISTRY_URL> <VERSION>"
+    echo "Use -p to build and push multiarch images"
+}
+
+PUSH="false"
+while getopts 'p' flag; do
+case "$flag" in
+    p) 
+        PUSH="true"
+        ;;
+    ?) 
+        print_usage >&2
+        exit 1
+        ;;
+esac
+done
+
+shift $((OPTIND-1))
 
 REGISTRY_URL="${1:-quay.io/datashim-io}"
 VERSION="${2:-latest}"
-docker buildx build --platform linux/amd64,linux/arm64,linux/ppc64le --network host  --push -t ${REGISTRY_URL}/csi-s3:${VERSION} -f ./cmd/s3driver/Dockerfile.full .
+
+docker_build () {
+    docker buildx build --platform linux/amd64 -t ${REGISTRY_URL}/csi-s3:${VERSION} --load -f cmd/s3driver/Dockerfile .
+}
+
+docker_build_and_push () {
+    docker buildx build --platform linux/amd64,linux/arm64,linux/ppc64le --push -t ${REGISTRY_URL}/csi-s3:${VERSION} -f cmd/s3driver/Dockerfile .
+}
+
+podman_build () {
+    podman manifest create ${REGISTRY_URL}/csi-s3:${VERSION}
+    podman buildx build --platform linux/amd64,linux/arm64  --manifest ${REGISTRY_URL}/csi-s3:${VERSION} cmd/s3driver/Dockerfile .
+}
+
+podman_push () {
+    podman manifest push ${REGISTRY_URL}/csi-s3:${VERSION} 
+ 
+}
+
+DOCKERCMD="docker"
+ALTDOCKERCMD="podman"
+if !(command -v ${DOCKERCMD} &> /dev/null)
+then
+    echo "Docker command not found"
+    if !(command -v ${ALTDOCKERCMD} &> /dev/null)
+    then
+        echo "Neither ${DOCKERCMD} nor ${ALTDOCKERCMD} commands found.. cannot build "
+        exit 1
+    else
+        DOCKERCMD=${ALTDOCKERCMD}  
+    fi
+else
+    echo "Docker command found"
+    cmd_type=$(type -t ${DOCKERCMD})
+    if [ $cmd_type == "alias" ]
+    then
+        echo "${DOCKERCMD} is an alias, switching to ${ALTDOCKERCMD}"
+        DOCKERCMD=${ALTDOCKERCMD}  
+    fi
+fi 
+
+if [ $PUSH == "true" ]
+then
+    echo "pushing images to the registry"
+    if [ ${DOCKERCMD} == "docker" ]
+    then
+        docker_build_and_push
+    else 
+        podman_build
+        podman_push
+    fi
+else
+    echo "building image locally"
+    if [ ${DOCKERCMD} == "docker" ]
+    then
+        docker_build 
+    else
+        podman_build
+    fi
+fi
